a111d9afa1a30beaafb9b439c8a3712c1c660c2d
DY <23586296+Kompensator@users.noreply.github.com>
2019-01-15 15:22:25 -0500

    working topup script that does F005

--- topup.sh
+++ topup.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+#####################################################
+# settings
+FULL_FILE="F005_EPIDiffusion"
+PA_FILE="F005_EPIDiffusionPA"
+B0_FILE="b0$FULL_FILE"
+MERGED_FILE="merged_$FULL_FILE"
+EXTRA_FILE="extra_$FULL_FILE"
+ACQ_PARAM="acq_param.txt"
+#####################################################
+
+`fslroi $FULL_FILE $B0_FILE 0 1`
+`fslmerge -t $MERGED_FILE $B0_FILE $PA_FILE`
+`fslroi $MERGED_FILE $EXTRA_FILE 0 -1 0 -1 0 66`
+`topup --imain=$EXTRA_FILE --datain=$ACQ_PARAM --config=b02b0.cnf --out="topup_$FULL_FILE" --iout="b0_topup_$FULL_FILE"`
