c37c9a0a798c044753d68c43a9def1bd4467ddfe
DY <23586296+Kompensator@users.noreply.github.com>
2019-01-16 20:07:33 -0500

    Added timing functionality

--- topup.sh
+++ topup.sh
@@ -10,7 +10,11 @@ EXTRA_FILE="extra_$FULL_FILE"
 ACQ_PARAM="acq_param.txt"
 ####################################################
 
+echo "topup of $FULL_FILE started at:"
+echo $(date +%r)
 `fslroi $FULL_FILE $B0_FILE 0 1`
 `fslmerge -t $MERGED_FILE $B0_FILE $PA_FILE`
 `fslroi $MERGED_FILE $EXTRA_FILE 0 -1 0 -1 0 66`
 `topup --imain=$EXTRA_FILE --datain=$ACQ_PARAM --config=b02b0.cnf --out="topup_$FULL_FILE" --iout="b0_topup_$FULL_FILE"`
+echo "topup of $FULL_FILE ended at:"
+echo $(date +%r)
