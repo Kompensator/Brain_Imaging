8f202626ecee188d3dcf40ce28c5b53c684d0702
DY <23586296+Kompensator@users.noreply.github.com>
2019-01-30 21:46:43 -0500

    Changes to make topup.sh more self evident

--- topup.sh
+++ topup.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
 
+# !!! MOVE THIS FILE TO WHERE YOU HAVE ALL THE DTI DATA !!!
+# to run the file:
+# 1. check the settings section below. make sure FULL_FILE and PA_FILE match your subject
+# 2. open command line
+# 3. type in "bash topup.sh"
+# 4. be patient xD
+
+####################################################   settings
+
+# change these file names to a different subject for topup to work
+# only 2 DTI files + the data acquisition parameter file are needed for topup (no bval, bvec..)
+
+FULL_FILE="F005_EPIDiffusion"   # this is the full DTI result file (usually the largest one in size)
+PA_FILE="F005_EPIDiffusionPA"   # this is the file that has "PA" in the name
+ACQ_PARAM="acq_param.txt"   # this must match the data acquisition file!
+
 ####################################################
-# settings
-FULL_FILE="F005_EPIDiffusion"
-PA_FILE="F005_EPIDiffusionPA"
+
+
 B0_FILE="b0$FULL_FILE"
 MERGED_FILE="merged_$FULL_FILE"
 EXTRA_FILE="extra_$FULL_FILE"
-ACQ_PARAM="acq_param.txt"
-####################################################
+
 
 echo "topup of $FULL_FILE started at:"
 echo $(date +%r)
+echo "this should take around 15 mins"
 `fslroi $FULL_FILE $B0_FILE 0 1`
 `fslmerge -t $MERGED_FILE $B0_FILE $PA_FILE`
 `fslroi $MERGED_FILE $EXTRA_FILE 0 -1 0 -1 0 66`
